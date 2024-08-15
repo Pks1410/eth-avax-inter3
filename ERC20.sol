26fb957d36b3d5293681a11d60d65777cf06fa43
Prakhar Sahu <129276455+Pks1410@users.noreply.github.com>
2024-08-16 01:23:12 +0530

    Create ERC20.sol

---
+++ ERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() Ownable(msg.sender) ERC20('Prakhar', 'PKS') {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+
+    function transferTokens(address recipient, uint256 amount) public returns (bool) {
+        return transfer(recipient, amount);
+    }
+}
